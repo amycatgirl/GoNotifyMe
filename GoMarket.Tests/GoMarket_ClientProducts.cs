7c69fcf4cf2ecad3f143152a66cd896904e93518
Amy <amycatgirlnya@outlook.com>
2023-08-13 10:56:26 -0400

    feat(tests): Add tests for GetVariantList and GetProduct methods

--- GoMarket.Tests/GoMarket_ClientProducts.cs
+++ GoMarket.Tests/GoMarket_ClientProducts.cs
@@ -24,7 +24,32 @@ namespace GoMarket.Tests
             Assert.IsInstanceOfType(result.Pages, typeof(int), "Pages should be a integer");
             Assert.IsInstanceOfType(result.TotalAmount, typeof(int), "TotalAmount should be a integer");
             Assert.IsInstanceOfType(result.ProductsPerPage, typeof(int), "ProductsPerPage should be a integer");
-            Assert.IsInstanceOfType(result.Products, typeof(List<ApiProduct>), "Products should be inside of a list");
+            Assert.IsInstanceOfType(result.Products, typeof(List<ApiProductListItem>), "Products should be inside of a list");
+        }
+
+        [TestMethod]
+        public void ClientProducts_GetDeserializedVariants()
+        {
+            ApiVariantList result = _client.GetVariantList();
+
+            Assert.IsNotNull(result, "Result should not be empty");
+            Assert.IsInstanceOfType(result.Count, typeof(int), "Count should be a integer");
+            Assert.IsInstanceOfType(result.CurrentPage, typeof(int), "CurrentPage should be a integer");
+            Assert.IsInstanceOfType(result.Pages, typeof(int), "Pages should be a integer");
+            Assert.IsInstanceOfType(result.TotalAmount, typeof(int), "TotalAmount should be a integer");
+            Assert.IsInstanceOfType(result.ProductsPerPage, typeof(int), "ProductsPerPage should be a integer");
+            Assert.IsInstanceOfType(result.Variants, typeof(List<ApiVariantListItem>), "Variants should be inside of a list");
+        }
+
+        [TestMethod]
+        [DataRow(630226)]
+        [DataRow(632003)]
+        [DataRow(631407)]
+        public void ClientProducts_GetDeserializedProduct(int Id)
+        {
+            var result = _client.GetProduct(Id);
+
+            Assert.IsInstanceOfType(result, typeof(ApiProduct), "Result should be a valid product.");
         }
     }
 }

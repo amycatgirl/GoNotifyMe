778679d5afa1fa8965330be7db9dcf309231f57f
Amy <amycatgirlnya@outlook.com>
2023-08-13 21:55:15 -0400

    feat(mail): Add basic mail client
    Right now, it can only send messages and connect to a smtp server.
    
    I want to also add functionality to generate messages on the fly instead of hardcoding everything.
    
    But I have to do it tomorrow, I am tired...

--- GoNotifyMe/Mail.cs
+++ GoNotifyMe/Mail.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
 
 namespace GoNotifyMe
 {
     internal class Mail
     {
+        private readonly string _ServerURL;
+        private readonly int _Port;
+        private readonly bool _UseSSL;
+
+        private readonly string _User;
+        private readonly string _Password;
+
+        public Mail(string url, int port, string user, string password, bool useSSL = true)
+        {
+            _ServerURL = url;
+            _Port = port;
+            _User = user;
+            _Password = password;
+            _UseSSL = useSSL;
+        }
+
+        public void SendMessage(MimeMessage message)
+        {
+            using (var client = new SmtpClient())
+            {
+                client.Connect(_ServerURL, _Port, SecureSocketOptions.SslOnConnect);
+
+                client.Authenticate(_User, _Password);
+
+                client.Send(message);
+
+                client.Disconnect(true);
+            }
+        }
     }
 }

644fc31d9ca245490c36cd59d33de6b4a7f2f536
Amy <amycatgirlnya@outlook.com>
2023-08-17 21:12:33 -0400

    feat(mail): Add Message Generation from template Ideally, I would want this on a separate file. But it will make localization difficult.
    Signed-off-by: Amy <amycatgirlnya@outlook.com>

--- GoNotifyMe/Mail.cs
+++ GoNotifyMe/Mail.cs
@@ -1,6 +1,10 @@
 ﻿using MailKit.Net.Smtp;
 using MailKit.Security;
+
 using MimeKit;
+using MimeKit.Text;
+
+using GoMarket;
 
 namespace GoNotifyMe
 {
@@ -13,13 +17,18 @@ namespace GoNotifyMe
         private readonly string _User;
         private readonly string _Password;
 
-        public Mail(string url, int port, string user, string password, bool useSSL = true)
+        private readonly Configuration _Config;
+
+        private readonly string Template = @"<h1>GoNotifyMe</h1>\n<p>These items need a restock:</p>";
+
+        public Mail(Configuration config, string url, int port, string user, string password, bool useSSL = true)
         {
             _ServerURL = url;
             _Port = port;
             _User = user;
             _Password = password;
             _UseSSL = useSSL;
+            _Config = config;
         }
 
         public void SendMessage(MimeMessage message)
@@ -34,6 +43,43 @@ namespace GoNotifyMe
 
                 client.Disconnect(true);
             }
+        }
+
+        private string generateTemplate(List<ApiProduct> products)
+        {
+            string HtmlList = "<ul>" + products.ToArray().Select(product => $"<li>{product.Name}: {product.InStock} in Stock</li>") + "</ul>";
+
+            return @$"
+            <h1>Some items require a restock</h1>
+            
+            <p>The following items need restock:</p>
+            
+            {HtmlList}";
+
+        }
+
+        public MimeMessage GenerateRestockMessage(List<ApiProduct> products)
+        {
+            // 1. Get products
+            // 2. Get current app configuration
+            // 3. Set mail recipients
+            // 4. Return a RestockMessage
+
+
+            var RestockMessage = new MimeMessage();
+
+
+
+            RestockMessage.From.Add(new MailboxAddress(null, _Config.Options!.Mail!.Email));
+            RestockMessage.To.Add(new MailboxAddress(null, _Config.Options!.TargetEmail));
+            RestockMessage.Subject = $"[GoNotifyMe] {products.Count} product{(products.Count > 1 ? "s" : String.Empty)} need restock";
+
+            RestockMessage.Body = new TextPart(TextFormat.Html)
+            {
+                Text = generateTemplate(products)
+            };
+
+            return RestockMessage;
         }
     }
 }
